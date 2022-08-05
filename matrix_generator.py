e46bf75219b4af158d69832ee1c21ce305f1c1cd
Raffaele <raffaele.villani17@gmail.com>
2022-08-05 08:43:33 +0200

    matrix generator script

--- matrix_generator.py
+++ matrix_generator.py
@@ -1,7 +1,14 @@
 import numpy as np
 import numpy.linalg as la
 
-def generate_matrix(n, cond_p=10**2):
+def generate_matrix(n, m, matrix_name):
+    PATH = './matrix/'
+    M1 = np.random.randn(n, m)
+    np.savetxt(PATH + "" + matrix_name + ".txt", M1)
+    print(" Matrix generated and saved.")
+
+
+def generate_cond_matrix(n, cond_p=10**2):
     # cond_P = 10**2     # Condition number
     log_cond_P = np.log(cond_p)
     exp_vec = np.arange(-log_cond_P/4., log_cond_P * (n)/(4 * (n - 1)), log_cond_P/(2.*(n-1)))
@@ -11,5 +18,10 @@ def generate_matrix(n, cond_p=10**2):
     V, _ = la.qr((np.random.rand(n, n) - 5.) * 200)
     P = U.dot(S).dot(V.T)
     P = P.dot(P.T)
+
+
+    PATH = './matrix/'
+    np.savetxt(PATH + "Matrix_cond.txt", P)
+    print(" Matrix generated and saved.")
 
     return P
